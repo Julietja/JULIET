673755d3548468c0df093a84fd930f7632226310
Oluchi Alaribe <bamarachi6@gmail.com>
2022-12-15 19:31:23 +0100

    Updated styles for about section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+let butnClick = document.querySelector('.dropbutn');
 let oldMenu = document.getElementById("old-menu");
 let newMenu = document.getElementById("new-menu");
 let showDropdown = document.getElementById("dropdown-list");
@@ -6,7 +8,7 @@ const changeOldMenu = () => {
  oldMenu.addEventListener("click", function(){
   oldMenu.style.display = 'none';
   newMenu.style.display = 'inline-block';
-  showDropdown.style.display = 'block'
+  showDropdown.style.display = 'block';
  }
  )
 }
@@ -14,10 +16,29 @@ changeOldMenu();
 
 const changeNewMenu = () => {
  newMenu.addEventListener("click", function(){
+ newMenu.style.display = 'none';
+ oldMenu.style.display = 'inline-block';
+ showDropdown.style.display = 'none';
+}
+)
+}
+
+changeNewMenu();
+
+
+
+
+
+/*let changeNewMenu = () => {
+
+ butnClick.addEventListener("click", function(){
+  newMenu = document.getElementById("new-menu");
   newMenu.style.display = 'none';
-  oldMenu.style.display = 'inline-block';
+  oldMenu.style.display = 'block';
   showDropdown.style.display = 'none';
  }
  )
 }
+
 changeNewMenu();
+*/
