db1a12911ec782edd55a14f6b75cd7b5f0682ce0
Oluchi Alaribe <105394637+Julietja@users.noreply.github.com>
2022-12-23 19:07:49 +0100

    Update contact-form-process.php

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,8 +1,6 @@
 <?php
 if (isset($_POST['Email'])) {
-
- 
-    $email_to = "bamarachi6@gmail.com";
+    $email_to = "admin@jayolatech.com";
     $email_subject = "New form submissions";
 
     function problem($error)
